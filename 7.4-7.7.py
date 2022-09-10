cc529d43b227b66ccecafc2c7f606c1ca10fd082
gbrinkerhoff <104337476+gbrinkerhoff@users.noreply.github.com>
2022-09-10 13:53:44 -0400

    Add files via upload

---
+++ 7.4-7.7.py
@@ -0,0 +1,10 @@
+# Name: Garrison Brinkerhoff
+# Assignment: M03_Programming assignment
+# Description: Chapter 7.4-7.7
+
+things = ['mozzarella','cinderella','salmonella']
+things[1] = 'Cinderella'
+things[0] = 'MOZZARELLA'
+del things[-1]
+print(things)
+
